cdf6e9379075dda459c8aeb78f2e615a605d668c
German <KostiakovG@Gmail.com>
2022-08-07 10:09:41 +0300

    configured get for http request

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client';
+  title: string = 'The Dating App';
+  users: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      // (response) => {
+      //   this.users = response;
+      // },
+      // (error) => {
+      //   console.log(error);
+      // },
+      // () => {
+      //   console.log('Finished');
+      // }
+       {
+        next: (response)=> {this.users = response;}, // what to do with returned data
+        error: (error) => {console.log(error);}, // what to do with error
+        complete: () => {console.log('Finished');} // what to do when finished
+       }
+    );
+  }
 }
